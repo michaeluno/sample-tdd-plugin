55e6d95ca0a8ba191f849743fefb6174a41b9f36
Michael Uno <michael@michaeluno.jp>
2015-03-06 01:22:44 +0900

    - Added a configuration file for the unit test installer script. - Made it not use wget. - Made it not use /tmp/ for the system temporary directory path.

--- test/bin/install-wp-tests.sh
+++ test/bin/install-wp-tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+if [ -f install-wp-tests.cfg ]; then
+    source install-wp-tests.cfg
+fi
+
 if [ $# -lt 3 ]; then
-	echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version]"
-	exit 1
+    echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version]"
+    exit 1
 fi
 
 DB_NAME=$1
@@ -11,66 +15,74 @@ DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 
-WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
-WP_CORE_DIR=/tmp/wordpress/
+WP_TESTS_DIR=${WP_TESTS_DIR-$TEMP/wordpress-tests-lib}
+WP_CORE_DIR=$TEMP/wordpress/
 
 set -ex
 
+download() {
+    if [ `which curl` ]; then
+        curl -s "$1" > "$2";
+    elif [ `which wget` ]; then
+        wget -nv -O "$2" "$1"
+    fi
+}
 install_wp() {
-	mkdir -p $WP_CORE_DIR
+    mkdir -p $WP_CORE_DIR
 
-	if [ $WP_VERSION == 'latest' ]; then 
-		local ARCHIVE_NAME='latest'
-	else
-		local ARCHIVE_NAME="wordpress-$WP_VERSION"
-	fi
+    if [ $WP_VERSION == 'latest' ]; then 
+        local ARCHIVE_NAME='latest'
+    else
+        local ARCHIVE_NAME="wordpress-$WP_VERSION"
+    fi
 
-	wget -nv -O /tmp/wordpress.tar.gz https://wordpress.org/${ARCHIVE_NAME}.tar.gz
-	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+    download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  $TEMP/wordpress.tar.gz
+    tar --strip-components=1 -zxmf $TEMP/wordpress.tar.gz -C $WP_CORE_DIR
 
-	wget -nv -O $WP_CORE_DIR/wp-content/db.php https://raw.github.com/markoheijnen/wp-mysqli/master/db.php
+    download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
+    
 }
 
 install_test_suite() {
-	# portable in-place argument for both GNU sed and Mac OSX sed
-	if [[ $(uname -s) == 'Darwin' ]]; then
-		local ioption='-i .bak'
-	else
-		local ioption='-i'
-	fi
+    # portable in-place argument for both GNU sed and Mac OSX sed
+    if [[ $(uname -s) == 'Darwin' ]]; then
+        local ioption='-i .bak'
+    else
+        local ioption='-i'
+    fi
 
-	# set up testing suite
-	mkdir -p $WP_TESTS_DIR
-	cd $WP_TESTS_DIR
-	svn co --quiet https://develop.svn.wordpress.org/trunk/tests/phpunit/includes/
+    # set up testing suite
+    mkdir -p $WP_TESTS_DIR
+    cd $WP_TESTS_DIR
+    svn co --quiet https://develop.svn.wordpress.org/trunk/tests/phpunit/includes/
 
-	wget -nv -O wp-tests-config.php http://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php
-	sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" wp-tests-config.php
-	sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
-	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
-	sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
-	sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+    download https://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php wp-tests-config.php
+    sed $ioption "s:dirname( __FILE__ ) . '/src/':getenv( 'TEMP' ) . '/wordpress/':" wp-tests-config.php
+    sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
+    sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
+    sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
+    sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
 }
 
 install_db() {
-	# parse DB_HOST for port or socket references
-	local PARTS=(${DB_HOST//\:/ })
-	local DB_HOSTNAME=${PARTS[0]};
-	local DB_SOCK_OR_PORT=${PARTS[1]};
-	local EXTRA=""
+    # parse DB_HOST for port or socket references
+    local PARTS=(${DB_HOST//\:/ })
+    local DB_HOSTNAME=${PARTS[0]};
+    local DB_SOCK_OR_PORT=${PARTS[1]};
+    local EXTRA=""
 
-	if ! [ -z $DB_HOSTNAME ] ; then
-		if [[ "$DB_SOCK_OR_PORT" =~ ^[0-9]+$ ]] ; then
-			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
-			EXTRA=" --socket=$DB_SOCK_OR_PORT"
-		elif ! [ -z $DB_HOSTNAME ] ; then
-			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
-		fi
-	fi
+    if ! [ -z $DB_HOSTNAME ] ; then
+        if [[ "$DB_SOCK_OR_PORT" =~ ^[0-9]+$ ]] ; then
+            EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
+        elif ! [ -z $DB_SOCK_OR_PORT ] ; then
+            EXTRA=" --socket=$DB_SOCK_OR_PORT"
+        elif ! [ -z $DB_HOSTNAME ] ; then
+            EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
+        fi
+    fi
 
-	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+    # create database
+    mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
 install_wp
