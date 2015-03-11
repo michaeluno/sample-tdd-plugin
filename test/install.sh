190960c303a3f08847cb4acaef27537ed1951e98
Michael Uno <michael@michaeluno.jp>
2015-03-11 14:55:50 +0900

    - Bumped the Codeception version to the latest (2.0.11). - Removed tests against PHP 5.3 as Codeception does not support it. - Removed tests against HHVM as wp-cli causes an error.

--- test/install.sh
+++ test/install.sh
@@ -254,26 +254,28 @@ installPlugin() {
 }
 
 downloadCodeception() {
-
-    # Use v1.8.x because v2.x does not support PHP 5.3
     
-    # latest
-    # download "http://codeception.com/codecept.phar" "$CODECEPT"        
+    # The Latest Version
+    download "http://codeception.com/codecept.phar" "$CODECEPT"        
     
-    # v1.8.7
-    download "http://codeception.com/releases/1.8.7/codecept.phar" "$CODECEPT"        
+    # v1.8.7 - causes an error " Path for logs is not writable. Please, set appropriate access mode for log"
+    # download "http://codeception.com/releases/1.8.7/codecept.phar" "$CODECEPT"        
+    
     if [[ ! $(find "$CODECEPT" -type f -size +0c 2>/dev/null) ]]; then
         echo Could not download Codeception.
         exit 1
     fi
+    # Output the version in case an error occurs.
+    php "$CODECEPT" --version      
+    
+    # c3 
+    # @see  https://github.com/Codeception/c3
     download "https://raw.github.com/Codeception/c3/2.0/c3.php" "$C3"
     if [[ ! $(find "$C3" -type f -size +0c 2>/dev/null) ]]; then
         echo Could not download c3.php.
         exit 1
-    fi    
-
-    # Output the version in case an error occurs.
-    php "$CODECEPT" --version    
+    fi
+  
     
 }
 
