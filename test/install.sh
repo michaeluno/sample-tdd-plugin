be4926eaf01e2497b0a113e46971353f76189722
Michael Uno <michael@michaeluno.jp>
2015-03-11 16:31:07 +0900

    Fixed an issue that the installer script installed WordPress multisite by default.

--- test/install.sh
+++ test/install.sh
@@ -115,7 +115,7 @@ PHP
     setup_database_table    
 
     # Create/renew the database - if the environment variable WP_MULTISITE is set, install multi site network Wordpress.
-    if [[ ! -z "$WP_MULTISITE" ]]; then    
+    if [[ $WP_MULTISITE ]]; then    
         php "$WP_CLI" wp core multisite-install --url="$WP_URL" --title="$WP_SITE_TITLE" --admin_user="$WP_ADMIN_USER_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
     else
         php "$WP_CLI" core install --url="$WP_URL" --title="$WP_SITE_TITLE" --admin_user="$WP_ADMIN_USER_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
