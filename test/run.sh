94133d593cb2f710820c5030b785d233bb7d2646
Michael Uno <michael@michaeluno.jp>
2015-03-15 08:32:26 +0900

    Made test suites run separately to avoid fatal error of redeclaring functions, classes and constants etc.

--- test/run.sh
+++ test/run.sh
@@ -86,7 +86,10 @@ if [[ ! -z "$COVERAGE_FILE_PATH" ]]; then
 else 
     OPTION_COVERAGE=
 fi
-php "$CODECEPT" run --report --colors --config="$CODECEPT_TEST_DIR" $OPTION_GROUP $OPTION_SKIP_GROUP $OPTION_COVERAGE
+
+php "$CODECEPT" run acceptance --report --colors --config="$CODECEPT_TEST_DIR" $OPTION_GROUP $OPTION_SKIP_GROUP
+php "$CODECEPT" run functional --report --colors --config="$CODECEPT_TEST_DIR" $OPTION_GROUP $OPTION_SKIP_GROUP $OPTION_COVERAGE
+php "$CODECEPT" run unit --report --colors --config="$CODECEPT_TEST_DIR" $OPTION_GROUP $OPTION_SKIP_GROUP
 
 # Copy the coverage file to the specified path
 if [[ ! -z "$COVERAGE_FILE_PATH" ]]; then
